2659e2bcd25d666ef26205eb6c612a3d79d1aa13
MrZloHex <zlo.alex.it@gmail.com>
2025-08-15 00:10:58 +0300

    bot

---
+++ cmd/luch/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+
+	"github.com/lmittmann/tint"
+	log "log/slog"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	log.SetDefault(log.New(
+		tint.NewHandler(os.Stdout, &tint.Options{
+			Level: log.LevelDebug,
+		}),
+	))
+
+	bot, err := tgbotapi.NewBotAPI("")
+	if err != nil {
+		log.Error("Failed to init bot", "err", err)
+	}
+
+	// bot.Debug = true
+
+	log.Info("BOOTING UP ON", "bot", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Info("Got msg", "from", update.Message.From.UserName, "text", update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
+}
